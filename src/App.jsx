7e07b6b909560b0561b5ec0c36102d4d53616394
awaisjbr <awaisjbr@gmail.com>
2025-02-22 10:05:10 +0500

    22-02-2025 v1

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 import { Route, Routes } from "react-router-dom";
 // import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Portfolio from "./pages/Portfolio";
-import Skills from "./pages/Skills";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+// import Portfolio from "./pages/Portfolio";
+// import Skills from "./pages/Skills";
 
 const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Skills = lazy(() => import("./pages/Skills"));
+const Portfolio = lazy(() => import("./pages/Portfolio"));
 
 const App = () => {
   return (
