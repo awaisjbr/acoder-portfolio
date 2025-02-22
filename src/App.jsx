6a5e1fd355a0741b98c872bae8452594c590a0c3
awaisjbr <awaisjbr@gmail.com>
2025-02-22 09:59:46 +0500

    22-02-2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import Skills from "./pages/Skills";
 
+const Home = lazy(() => import("./pages/Home"));
 
 const App = () => {
   return (
-    <div className="relative bg-[rgb(108,78,232)] min-h-screen flex items-center justify-center py-1">
-      <div className="bg-white h-[700px] w-[90%] rounded-3xl p-3 my-3 overflow-hidden">
-        <Navbar />
-        {/* <Intro /> */}
-        <div className="overflow-y-auto h-full">
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/about" element={<About />}/>
-            <Route path="/contact" element={<Contact />}/>
-            <Route path="/portfolio" element={<Portfolio />}/>
-            <Route path="/skills" element={<Skills />}/>
-          </Routes>
-        </div>
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Suspense fallback={"Loading..."}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/skills" element={<Skills />} />
+        </Routes>
+      </Suspense>
+    </>
   );
 };
 
